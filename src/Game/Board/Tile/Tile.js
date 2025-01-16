286e2ac13511b1ea58399643f1166edcd6333fd8
Chandan <Chandan17ee@gmail.com>
2025-01-16 11:22:52 +0530

    chore:integrated tile component

--- src/Game/Board/Tile/Tile.js
+++ src/Game/Board/Tile/Tile.js
@@ -1,12 +1,19 @@
 import React, { memo, useEffect, useRef } from "react";
 import styles from "./styles.module.css";
-import {useSelector, useDispatch, shallowEqual} from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { motion } from "framer-motion";
 import marks from "./marks";
 
 const Tile = ({ row, column, variants }) => {
   const markRef = useRef();
   const tileRef = useRef();
+  const dispatch = useDispatch();
+  const tile = useSelector((state) => state?.board?.tiles[row][column]);
+  const winningTiles = useSelector(
+    (state) => state?.board?.winningTiles,
+    shallowEqual
+  );
+  const turn = useSelector((state) => state?.turn);
 
   const childVariants = {
     hidden: { opacity: 0 },
@@ -19,6 +26,38 @@ const Tile = ({ row, column, variants }) => {
     // dispatch({type: 'CHECK_DRAW'});
     // dispatch({type: 'CHANGE_TURN'});
   };
+
+  /* The `useEffect` hook in the code snippet is responsible for updating the image source of the mark
+  displayed on the tile based on the current turn and the value of the tile in the game board state. The useEffect that decides which icon to display when the user hovers over the tile */
+  useEffect(() => {
+    if (tile !== 0) {
+      return;
+    }
+
+    markRef.current.src =
+      turn === "x" ? marks["hoverIconX"] : marks["hoverIconO"];
+  }, [turn, tile]);
+
+
+
+
+/* The `useEffect` hook in the provided code snippet is responsible for updating the appearance of the
+tile based on changes in the `tile` and `turn` variables. Here's a breakdown of what it does: */
+  useEffect(()=>{
+
+    if(tile===0){   // tile has been reset
+      tileRef.current.style.pointerEvents=''
+      tileRef.current.style.backgroundColor = '';   
+      markRef.current.src= turn ==='x' ? marks["hoverIconX"] : marks["hoverIconO"]
+      markRef.current.style.transform = ''
+    }else{  // tile has been selected
+      tileRef.current.style.pointerEvents='none'
+      markRef.current.src = tile==='x' ? marks['iconX'] : marks['iconO']
+      markRef.current.style.transform ='scale(1'
+    }
+
+  },[tile,turn])
+
 
   return (
     <motion.div
--- src/Store/Reducers/turnReducer.js
+++ src/Store/Reducers/turnReducer.js
@@ -2,7 +2,7 @@ export default function turnReducer(turn = "x", action) {
   switch (action.type) {
     case "CHANGE_TURN":
       return turn === "x" ? "o" : "x";
-      
+
     case "NEW_GAME":
     case "RESTART":
       return "x";
@@ -10,3 +10,7 @@ export default function turnReducer(turn = "x", action) {
       return turn;
   }
 }
+
+/**
+ * In My turnReducer function, the NEW_GAME case is included in the switch statement, but you haven't provided any specific logic for it beyond what is already defined in the RESTART case. This means that when the action type is NEW_GAME, the reducer will execute the same logic as for the RESTART case, which is to reset the turn to 'x'
+ */
